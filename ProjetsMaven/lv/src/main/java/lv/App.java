9f455b823086405b13146954f3ec91ee278cad9b
Ville Loic <lville@SA203-07A.ensi-bourges.fr>
2017-04-12 19:22:38 +0200

    un TD bien terminéé

--- ProjetsMaven/lv/src/main/java/lv/App.java
+++ ProjetsMaven/lv/src/main/java/lv/App.java
@@ -1,5 +1,19 @@
 package lv;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.collections4.Predicate;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
 /**
  * Hello world!
  *
@@ -11,11 +25,45 @@ public class App
         System.out.println( "Cocaine!" );
         int maximum=0;
         App app=new App();
-        maximum=app.max(4,5);
-        System.out.println( maximum );
+        
+        try {
+        	CSVReader reader = new CSVReader(new FileReader("data.csv"),',');
+        	CSVWriter writer = new CSVWriter(new FileWriter("data2.scv"), ',', CSVWriter.NO_QUOTE_CHARACTER);
+        	List<String[]>myEntries=reader.readAll();
+        	//maximum=app.max(4,5);
+        	for (String[] string : myEntries){
+                Vector<String> out = new Vector<String>() ;
+        		List<String> list2 = Arrays.asList(string);
+        		CollectionUtils.select(list2, new Predicate<String>(){
+
+					public boolean evaluate(String arg0) {
+						if (Integer.parseInt(arg0) > 50){
+						return false;
+						}
+						else{
+							return true;
+						}
+					}
+        			
+        		}, out) ;
+        		System.out.println("OUT :" + out) ;
+                String[] vout=out.toArray(new String[0]);
+                
+                writer.writeNext(vout);
+        		for (String nb : string){
+        			maximum=app.max(maximum,Integer.parseInt(nb));
+        			System.out.println("Nombre lu: "+Integer.parseInt(nb));
+        		}
+        	}
+        	System.out.println("maximum: "+maximum);
+            reader.close();
+            writer.close();
+        } catch (IOException e){
+        	System.out.println("Erreur d ouverture fichier");
+        }
     }
     public int max(int a, int b){
-    	//return a > b ? a: b;
-    	return a;
+    	return a > b ? a: b;
+    	//return a;
     }
 }
